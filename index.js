7ffac98e4642350dc02e14109b25ca2587665d74
Dhiraj-4 <dhirajdipaklondhe4@gmail.com>
2025-11-01 20:25:45 +0530

    updated version

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const program = new Command();
 program
   .name("spawnflow")
   .description("A CLI tool to manage and launch spawnflow workspaces")
-  .version("1.0.0");
+  .version("1.0.1");
 
 // ✅ create command
 program
